61983491bd7b1e96ce8883baa2e8851db35c6b30
Klaus Ahrens <ahrens@informatik.hu-berlin.de>
2011-08-10 13:42:03 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hi
+//
+//  Created by Klaus Ahrens on 10.08.11.
+//  Copyright 2011 HU Berlin. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hi there\n";
+    return 0;
+}
+
